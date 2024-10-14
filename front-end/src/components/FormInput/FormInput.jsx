17852474f0e770e85183260cca1fd152bc0db424
CDoctor00 <capriomattia@gmail.com>
2024-10-14 17:53:29 +0000

    FE [FEAT] - Added handling of the response value

--- front-end/src/components/FormInput/FormInput.jsx
+++ front-end/src/components/FormInput/FormInput.jsx
@@ -1,46 +1,92 @@
+import { useState } from "react";
+
 function FormInput() {
-  return (
-    <div className="FormInput">
-      <form method="post" onSubmit={handleSubmit}>
-        <label>
-          Paste your link here:
-          <input name="url" placeholder="www.styk.com" />
-        </label>
-        <label>
-          Select the duration:
-          <input name="timeToLive" type="number" placeholder="100000" />
-        </label>
-        <button type="submit">Shorten URL</button>
-        <button type="reset">Reset</button>
-      </form>
-    </div>
-  );
-}
+  const [shortURL, setShortURL] = useState("");
+  const [originalURL, setOriginalURL] = useState("");
+  const [buttonText, setButtonText] = useState("Copy to clipboard");
 
-let handleSubmit = async (event) => {
-  event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-  let { url, timeToLive } = event.target;
-  const body = {
-    url: url.value,
-    timeToLive: Number(timeToLive.value),
+    let { url, timeToLive } = event.target;
+    const body = {
+      url: url.value,
+      timeToLive: Number(timeToLive.value),
+    };
+
+    fetch("http://192.168.0.102:10000/shorten", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Request rejected");
+        }
+        return response.json();
+      })
+      .then((response) => {
+        setOriginalURL(response.originalURL);
+        setShortURL(response.shortURL);
+      })
+      .catch((data) => {
+        console.error(`Fetch error: ${data}`);
+      });
   };
 
-  fetch("http://192.168.0.102:10000/shorten", {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Request rejected");
-      }
-    })
-    .catch((err) => {
-      console.error(`Fetch error: ${err}`);
-    });
-};
+  const copyURL = (event) => {
+    let url = event.target.name === "short" ? shortURL : originalURL;
+
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        setButtonText("Copied to clipboard");
+        setTimeout(function () {
+          setButtonText("Copy to clipboard");
+        }, 1000);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  if (shortURL !== "") {
+    return (
+      <div className="Output">
+        <div>
+          <p>This is your short URL: {shortURL}</p>
+          <button name="short" onClick={copyURL}>
+            {buttonText}
+          </button>
+        </div>
+        <div>
+          <p>This is your original URL: {originalURL}</p>
+          <button name="original" onClick={copyURL}>
+            {buttonText}
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="FormInput">
+        <form method="post" onSubmit={handleSubmit}>
+          <label>
+            Paste your link here:
+            <input name="url" placeholder="www.styk.com" />
+          </label>
+          <label>
+            Select the duration:
+            <input name="timeToLive" type="number" placeholder="100000" />
+          </label>
+          <button type="submit">Shorten URL</button>
+          <button type="reset">Reset</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default FormInput;
