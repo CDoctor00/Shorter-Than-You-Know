c53191ff40a2d98d30fd6fb49ad6fc4d85d9cee6
Mattia Caprio <capriomattia@gmail.com>
2025-02-20 15:08:30 +0100

    [BUILD] - Add nix package managerdependencies files

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          system = system;
+        };
+        pkgs-unstable = import nixpkgs-unstable {
+          system = system;
+        };
+
+        common = with pkgs; [
+          nodejs_23
+          go
+        ];
+
+        unstable = with pkgs-unstable; [
+          pnpm
+        ];
+
+        # runtime Deps
+        libraries = with pkgs;
+          [
+          ]
+          ++ common;
+
+        # compile-time deps
+        packages = with pkgs;
+          [
+          ]
+          ++ common ++ unstable;
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = packages;
+          buildInputs = libraries;
+          shellHook = ''
+            echo "🚀 Development environment loaded"
+          '';
+        };
+
+        formatter = pkgs.alejandra;
+      }
+    );
+}
