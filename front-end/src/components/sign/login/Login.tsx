7d652150ab6f304843e2f37839d2d952ae062c62
Mattia Caprio <capriomattia@gmail.com>
2025-02-27 21:20:53 +0100

    FE [FEAT] - Add Signup and Login functions

--- front-end/src/components/sign/login/Login.tsx
+++ front-end/src/components/sign/login/Login.tsx
@@ -1,5 +1,12 @@
 import { FaFacebook, FaGithub, FaGoogle, FaLinkedin } from "react-icons/fa6";
 import "./Login.css";
+import { z } from "zod";
+import { useRef } from "react";
+
+type LoginRequestBody = {
+  email: string;
+  password: string;
+};
 
 function LoginForm({
   isOpen,
@@ -8,6 +15,58 @@ function LoginForm({
   isOpen: boolean;
   toggleForm: () => void;
 }) {
+  const ref = useRef<HTMLFormElement | null>(null);
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const formValues = Object.fromEntries(formData);
+
+    const formSchema = z.object({
+      email: z.string({ message: "Email error" }).email(),
+      password: z.string({ message: "Password error" }).min(1),
+    });
+
+    const resultsForm = formSchema.safeParse(formValues);
+    if (!resultsForm.success) {
+      console.error(resultsForm.error);
+      return;
+    }
+
+    const body: LoginRequestBody = {
+      email: resultsForm.data.email,
+      password: resultsForm.data.password,
+    };
+
+    const response = await fetch("http://localhost:10000/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+
+    const responseData = await response.json();
+    if (!response.ok) {
+      console.error(response);
+    }
+
+    const responseSchema = z.object({
+      accessToken: z.string({ message: "accessToken error" }).min(1),
+      refreshToken: z.string({ message: "refreshToken error" }).min(1),
+    });
+
+    const resultsResponse = responseSchema.safeParse(responseData);
+    if (!resultsResponse.success) {
+      console.error(resultsResponse.error);
+      return;
+    }
+
+    sessionStorage.setItem("access_token", resultsResponse.data.accessToken);
+    localStorage.setItem("refresh_token", resultsResponse.data.refreshToken);
+
+    ref.current?.reset();
+  };
+
   return (
     <div
       className={`card-container down ${isOpen ? "open" : "close"}`}
@@ -19,7 +78,7 @@ function LoginForm({
       >
         Sign In
       </label>
-      <form>
+      <form onSubmit={onSubmit} ref={ref}>
         <div className="input-container">
           <input type="email" name="email" placeholder="Email" />
           <input type="password" name="password" placeholder="Password" />
--- front-end/src/components/sign/signup/Signup.tsx
+++ front-end/src/components/sign/signup/Signup.tsx
@@ -1,5 +1,14 @@
 import { FaFacebook, FaGithub, FaGoogle, FaLinkedin } from "react-icons/fa6";
 import "./Signup.css";
+import { useRef } from "react";
+import { z } from "zod";
+
+type SignupRequestBody = {
+  email: string;
+  password: string;
+  name?: string;
+  surname?: string;
+};
 
 function SignUp({
   isOpen,
@@ -8,6 +17,54 @@ function SignUp({
   isOpen: boolean;
   toggleForm: () => void;
 }) {
+  const ref = useRef<HTMLFormElement | null>(null);
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const formValues = Object.fromEntries(formData);
+
+    const formSchema = z.object({
+      email: z.string({ message: "Email error" }).email(),
+      password1: z.string({ message: "Password 1 error" }).min(1),
+      password2: z.string({ message: "Password 2 error" }).min(1),
+      name: z.string({ message: "Name error" }).optional(),
+      surname: z.string({ message: "Surname error" }).optional(),
+    });
+
+    const resultsForm = formSchema.safeParse(formValues);
+    if (!resultsForm.success) {
+      console.error(resultsForm.error);
+      return;
+    }
+
+    if (resultsForm.data.password1 !== resultsForm.data.password2) {
+      console.error("The two passwords don't match");
+      return;
+    }
+
+    const body: SignupRequestBody = {
+      email: resultsForm.data.email,
+      password: resultsForm.data.password1,
+      name: resultsForm.data.name,
+      surname: resultsForm.data.surname,
+    };
+
+    const response = await fetch("http://localhost:10000/api/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      console.error(response);
+      return;
+    }
+
+    ref.current?.reset();
+  };
+
   return (
     <div className="card-container">
       <label
@@ -16,15 +73,15 @@ function SignUp({
       >
         Create Account
       </label>
-      <form>
+      <form ref={ref} onSubmit={onSubmit}>
         <div className="input-container">
           <input type="text" name="name" placeholder="Name" />
           <input type="text" name="surname" placeholder="Surname" />
           <input type="email" name="email" placeholder="Email*" />
-          <input type="password" name="password-1" placeholder="Password*" />
+          <input type="password" name="password1" placeholder="Password*" />
           <input
             type="password"
-            name="password-2"
+            name="password2"
             placeholder="Repeat Password*"
           />
         </div>
