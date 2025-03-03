13206697f4abe2181ad7caef65d1585d2b0c7780
Mattia Caprio <capriomattia@gmail.com>
2025-03-03 16:51:17 +0100

    FE [FEAT] - Add error page

---
+++ front-end/src/components/error/Page.tsx
@@ -0,0 +1,12 @@
+import Image from "./Image";
+import "./Page.css";
+
+function ErrorPage() {
+  return (
+    <div className="error-page">
+      <Image />
+    </div>
+  );
+}
+
+export default ErrorPage;
