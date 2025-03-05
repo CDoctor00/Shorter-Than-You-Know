5c7994aa48e934169b735e75f4a607ac79e8945f
Mattia Caprio <capriomattia@gmail.com>
2025-03-05 19:03:31 +0100

    FE [FIX] - Solve not set theme from localStorage correctly

--- front-end/src/contexts/theme/Provider.tsx
+++ front-end/src/contexts/theme/Provider.tsx
@@ -1,51 +1,56 @@
 import { useEffect, useState } from "react";
 import { ThemeContext } from "./Context";
 
-const themeAttribute = "data-theme";
+const attributeName = "data-theme";
 const localStorageVariable = "site-theme";
+const lightTheme = "light";
+const darkTheme = "dark";
 
 interface props {
   children: React.ReactNode;
 }
 
 const ThemeContextProvider = (props: props) => {
-  const [isLightTheme, setIsLightTheme] = useState(true);
-
-  const themeSwitcher = () => {
-    const htmlTag = document.getElementsByTagName("html")[0];
-
-    const theme = htmlTag.getAttribute(themeAttribute);
-    if (theme === "light") {
-      htmlTag.setAttribute(themeAttribute, "dark");
-      window.localStorage.setItem(localStorageVariable, "dark");
-      setIsLightTheme(false);
-    } else {
-      htmlTag.setAttribute(themeAttribute, "light");
-      window.localStorage.setItem(localStorageVariable, "light");
-      setIsLightTheme(true);
-    }
-  };
+  const [theme, setTheme] = useState<undefined | string>();
 
   useEffect(() => {
     const lsTheme = window.localStorage.getItem(localStorageVariable);
 
     if (lsTheme === null) {
-      const htmlTag = document.getElementsByTagName("html")[0];
-
-      //? Checking if the browser theme
+      // //? Checking the browser theme
       if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        htmlTag.setAttribute(themeAttribute, "dark");
-        window.localStorage.setItem(localStorageVariable, "dark");
-        setIsLightTheme(false);
-      } else {
-        htmlTag.setAttribute(themeAttribute, "light");
-        window.localStorage.setItem(localStorageVariable, "light");
+        setTheme(darkTheme);
       }
+    } else {
+      setTheme(lsTheme);
     }
   }, []);
 
+  useEffect(() => {
+    if (!theme) {
+      return;
+    }
+
+    const htmlTag = document.getElementsByTagName("html")[0];
+    htmlTag.setAttribute(attributeName, theme);
+    window.localStorage.setItem(localStorageVariable, theme);
+  }, [theme]);
+
+  const themeSwitcher = () => {
+    const htmlTag = document.getElementsByTagName("html")[0];
+
+    const themeAttribute = htmlTag.getAttribute(attributeName);
+    if (themeAttribute === lightTheme) {
+      setTheme(darkTheme);
+    } else {
+      setTheme(lightTheme);
+    }
+  };
+
   return (
-    <ThemeContext.Provider value={{ isLightTheme, themeSwitcher }}>
+    <ThemeContext.Provider
+      value={{ isLightTheme: theme === lightTheme, themeSwitcher }}
+    >
       {props.children}
     </ThemeContext.Provider>
   );
